eed34f578bea5bef73ddeb3882746bcaaf2e3870
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-03-16 12:17:14 -0300

    crea rutas iniciales

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App () {
   return (
     <div className="App">
       <BrowserRouter>
-        <h1>React Router</h1>
+        <Routes>
+          <Route path="/" element={ <h1>Todas las pizzas</h1> } />
+          <Route path=":pizzaId" element={ <h1>Una pizza</h1> } />
+          <Route path="*" element={ <h1>404</h1> } />
+        </Routes>
       </BrowserRouter>
     </div>
   );
