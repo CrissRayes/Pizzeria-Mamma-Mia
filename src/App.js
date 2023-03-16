3054fdec4072450dd18527a57184025717a2061d
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-03-16 12:32:37 -0300

    actualiza rutas de app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, Pizza, NotFound } from './views'
 
 function App () {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={ <h1>Todas las pizzas</h1> } />
-          <Route path=":pizzaId" element={ <h1>Una pizza</h1> } />
-          <Route path="*" element={ <h1>404</h1> } />
+          <Route path="/" element={ <Home /> } />
+          <Route path=":pizzaId" element={ <Pizza /> } />
+          <Route path="*" element={ <NotFound /> } />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/views/index.js
@@ -0,0 +1,4 @@
+export * from './Home'
+export * from './Pizza'
+export * from './NotFound'
+export * from './Cart'
