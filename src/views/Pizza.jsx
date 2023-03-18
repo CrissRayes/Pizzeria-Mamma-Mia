225c56f9f74590b7ebfe2a6594eb9c3137622b39
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-03-18 12:22:05 -0300

    crea vista detalle de pizza

--- src/views/Pizza.jsx
+++ src/views/Pizza.jsx
@@ -1,7 +1,44 @@
+import { Container } from 'react-bootstrap'
+import { FaCartPlus } from 'react-icons/fa'
+import img1080x1080 from '../assets/img/img1080x1080.png'
+
 export const Pizza = () => {
   return (
-    <div>
-      <h1>Pizza segun su id</h1>
-    </div>
+    <Container>
+      <div className='row'>
+        <div className='col-6'>
+          <img
+            src={img1080x1080}
+            alt='pizza'
+            className='img-fluid'
+          />
+        </div>
+        <div className='col-6'>
+          <h2 className='border-bottom'>Pizza</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptas, quod, quia, voluptates quae voluptatibus quibusdam
+            voluptatum quos quas quidem nesciunt. Quisquam, quae. Quisquam
+            voluptas, quod, quia, voluptates quae voluptatibus quibusdam
+            voluptatum quos quas quidem nesciunt. Quisquam, quae.
+          </p>
+          <h4>Ingredientes:</h4>
+          <ul>
+            <li>Queso</li>
+            <li>Tomate</li>
+            <li>Pepperoni</li>
+            <li>Salchicha</li>
+            <li>Pollo</li>
+          </ul>
+          <div className='d-flex justify-content-between'>
+            <h5>Precio: $20.990</h5>
+            <button className='btn btn-primary'>
+              Añadir
+              <FaCartPlus />
+            </button>
+          </div>
+        </div>
+      </div>
+    </Container>
   )
 }
