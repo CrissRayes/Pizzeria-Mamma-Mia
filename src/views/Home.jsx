9f337ce6689f3f46c3c06e0327a17a85eabd4765
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-03-18 18:13:46 -0300

    pasa props id a Card

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,13 +4,13 @@ import { Container } from 'react-bootstrap'
 
 export const Home = () => {
   const { data } = useData()
-
   return (
     <Container>
       <div className='row row-cols-1 row-cols-md-4 g-4'>
         {data.map(pizza => (
           <Card
             key={pizza.id}
+            id={pizza.id}
             nombre={pizza.nombre}
             precio={pizza.precio}
             descripcion={pizza.descripcion}
