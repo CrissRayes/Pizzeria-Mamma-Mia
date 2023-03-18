31028b5f5e9b7bb65c98da8fceb566344b11df7a
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-03-18 18:13:17 -0300

    agrega useParams

--- src/views/Pizza.jsx
+++ src/views/Pizza.jsx
@@ -1,8 +1,11 @@
 import { Container } from 'react-bootstrap'
 import { FaCartPlus } from 'react-icons/fa'
 import img1080x1080 from '../assets/img/img1080x1080.png'
+import { useParams } from 'react-router-dom'
 
 export const Pizza = () => {
+  const { id } = useParams()
+
   return (
     <Container>
       <div className='row'>
@@ -14,7 +17,7 @@ export const Pizza = () => {
           />
         </div>
         <div className='col-6'>
-          <h2 className='border-bottom'>Pizza</h2>
+          <h2 className='border-bottom'>Pizza{id}</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
             voluptas, quod, quia, voluptates quae voluptatibus quibusdam
