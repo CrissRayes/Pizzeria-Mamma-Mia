f9c259ffddbef54d9837f6bc04884f4bbe641df3
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-03-19 09:26:07 -0300

    ya renderiza nombre de la pizza dinamicamente

--- src/views/Pizza.jsx
+++ src/views/Pizza.jsx
@@ -2,9 +2,24 @@ import { Container } from 'react-bootstrap'
 import { FaCartPlus } from 'react-icons/fa'
 import img1080x1080 from '../assets/img/img1080x1080.png'
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 export const Pizza = () => {
   const { id } = useParams()
+  const [pizza, setPizza] = useState({})
+
+  // TODO: Fetch data from API using id
+  const getPizza = async () => {
+    const url = 'http://localhost:3000/pizzas.json'
+    const response = await fetch(url)
+    const data = await response.json()
+    const pizzaMatch = data.find(pizza => pizza.id === id)
+    setPizza(pizzaMatch)
+  }
+
+  useEffect(() => {
+    getPizza()
+  }, [])
 
   return (
     <Container>
@@ -18,6 +33,8 @@ export const Pizza = () => {
         </div>
         <div className='col-6'>
           <h2 className='border-bottom'>Pizza{id}</h2>
+          <h2>{pizza?.name || ''}</h2>
+          <p>{pizza.name}</p>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
             voluptas, quod, quia, voluptates quae voluptatibus quibusdam
