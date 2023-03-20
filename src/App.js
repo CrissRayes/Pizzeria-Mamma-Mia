c163daf846a2677869ed7853c08931bfe090cb26
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-03-20 20:47:15 -0300

    define estado global de la data

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import { Carousel, Footer, Nvbar } from './components'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home, Pizza, NotFound, Cart } from './views'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import CartContext from './my_context'
+import getData from './helpers/getData'
+
 
 
 function App () {
 
+  const [data, setData] = useState( [] )
   const [cart, setCart] = useState( [] )
+
+  const getPizzas = async () => {
+    const pizzas = await getData()
+    setData( pizzas )
+  }
+
+  useEffect( () => {
+    getPizzas()
+  }, [] )
+
+  const sharedStates = {
+    data,
+    setData,
+    cart,
+    setCart,
+  }
 
   return (
     <div className="App">
-      <CartContext.Provider value={ { cart, setCart } }>
+      <CartContext.Provider value={ sharedStates }>
         <BrowserRouter>
           <Nvbar />
           <Carousel />
